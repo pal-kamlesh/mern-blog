bc4a1ca3c0dbf08042816c1d5878abff2f420dac
Kamlesh Pal <kamleshiit5@gmail.com>
2024-01-05 16:26:26 +0530

    connect to the database

--- api/index.js
+++ api/index.js
@@ -1,7 +1,16 @@
 import express from "express";
-
-const  app = express();
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
+console.log(process.env.MONGO);
+mongoose
+  .connect(`${process.env.MONGO}mern-blog`)
+  .then(() => console.log("DB connected"))
+  .catch((err) => {
+    console.log(err);
+  });
+const app = express();
 
 app.listen(3000, () => {
-    console.log('Server is running on port 3000');
-})
+  console.log("Server is running on port 3000");
+});
