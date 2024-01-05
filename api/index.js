ee7671508ef7c636cda3b1b8f1b667347af2c171
Kamlesh Pal <kamleshiit5@gmail.com>
2024-01-05 17:13:09 +0530

    create a test api route

--- api/index.js
+++ api/index.js
@@ -1,8 +1,9 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+import userRoutes from "./routes/user.route.js";
 dotenv.config();
-console.log(process.env.MONGO);
+
 mongoose
   .connect(`${process.env.MONGO}mern-blog`)
   .then(() => console.log("DB connected"))
@@ -10,6 +11,8 @@ mongoose
     console.log(err);
   });
 const app = express();
+
+app.use("/api/user", userRoutes);
 
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
