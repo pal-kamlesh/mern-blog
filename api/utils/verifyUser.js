07ec3d40ae4d349c829f9a5d6d88b3777269e864
Kamlesh Pal <kamleshiit5@gmail.com>
2024-01-16 12:00:59 +0530

    add deletet user api route

--- api/utils/verifyUser.js
+++ api/utils/verifyUser.js
@@ -4,11 +4,11 @@ import { errorHandler } from "./error.js";
 export const verifyToken = (req, res, next) => {
   const token = req.cookies.access_token;
   if (!token) {
-    return next(errorHadler(401, "unauthorized"));
+    return next(errorHandler(401, "unauthorized"));
   }
   jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
     if (err) {
-      return next(errorHadler(401, "unauthorized"));
+      return next(errorHandler(401, "unauthorized"));
     }
     req.user = user;
     next();
