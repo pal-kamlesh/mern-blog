d3b6cb0a3cbb9fdc4818317c2e7253d46412b870
Kamlesh Pal <kamleshiit5@gmail.com>
2024-01-20 19:38:24 +0530

    complete about and projects pages

--- client/src/pages/Search.jsx
+++ client/src/pages/Search.jsx
@@ -48,6 +48,7 @@ export default function Search() {
       }
     };
     fetchPosts();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [location.search]);
 
   const handleChange = (e) => {
