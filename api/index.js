63c3ce2f80d5e691805ab7946a86e2762a36f81e
pal-kamlesh <kamleshiit5@gmail.com>
2025-08-10 18:30:26 +0530

    little chagnes

--- api/index.js
+++ api/index.js
@@ -54,6 +54,7 @@ app.use((err, req, res, next) => {
     message,
   });
 });
-app.listen(5000, () => {
-  console.log("Server is running on port 5000");
+const port = process.env.PORT || 5000;
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
 });
